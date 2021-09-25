da5310d0dbfe2c0aa71e36e9a6f862e12b8efffe
daksindu <daksinducosta@gmail.com>
2021-09-25 20:12:53 +0530

    create register and userprofile

---
+++ app/src/main/java/com/example/pickpacklogin/UserHelperClass.java
@@ -0,0 +1,57 @@
+package com.example.pickpacklogin;
+
+public class UserHelperClass {
+
+    String name, address, password, phoneNumber, email;
+
+    public UserHelperClass() {
+    }
+
+    public UserHelperClass(String name, String address, String password, String phoneNumber, String email) {
+        this.name = name;
+        this.address = address;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
