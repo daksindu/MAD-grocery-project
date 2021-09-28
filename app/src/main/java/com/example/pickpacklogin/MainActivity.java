b7a4d56ed627934bd645c37365eda580cc5d1965
ezonline <ezonline00@gmail.com>
2021-09-28 20:37:48 +0530

    add to cart

--- app/src/main/java/com/example/pickpacklogin/MainActivity.java
+++ app/src/main/java/com/example/pickpacklogin/MainActivity.java
@@ -101,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     private void sendUserToNextActivity() {
-        Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+        Intent intent=new Intent(MainActivity.this,CartActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
